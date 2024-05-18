164dceaceff5383e35fb9dd8a85d74ffa7b535cd
mshkveron <166385926+mshkveron@users.noreply.github.com>
2024-05-18 15:11:49 +0500

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,50 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'phpmailer/src/Exception.php';
+    require 'phpmailer/src/PHPMailer.php';
+
+    $mail = new PHPMailer(true);
+    $mail->CharSet = 'UTF-8';
+    $mail->setLanguage('ru', 'phpmailer/language/');
+    $mail->IsHTML(true);
+
+    // От кого письмо
+    $mail->setFrom('mshk_veron@mail.ru', 'ОренГофроТара заявка');
+    // Кому отправить
+    $mail->addAddress('vermesh_2@mail.ru');
+    // Тема письма
+    $mail->Subject = 'Заявка на расчет стоимости';
+
+    // Тело письма
+    $body = '<h1>Новая заявка на расчет стоимости</h1>';
+
+    if(trim(!empty($_POST['name']))) {
+        $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+    }
+    if(trim(!empty($_POST['email']))) {
+        $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+    }
+    if(trim(!empty($_POST['age']))) {
+        $body.='<p><strong>Возраст:</strong> '.$_POST['age'].'</p>';
+    }
+    if(trim(!empty($_POST['message']))) {
+        $body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+    }
+
+    $mail->Body = $body;
+
+    // Отправляем
+    if (!$mail->send()) {
+        $message = 'Ошибка';
+    } else {
+        $message = 'Данные отправлены!';
+    }
+
+    $response = ['message' => $message];
+
+    header('Content-type: application/json');
+    echo json_encode($response);
+
+?>
